8b81125a8b7b1311c2f26d5103777769e541c3f0
rjcalifornia <rodolfohernan20@gmail.com>
2024-07-25 15:27:05 -0600

    Changes to be committed: 	modified:   .env.example 	modified:   app/Http/Controllers/api/MainController.php 	modified:   routes/api.php

--- app/Http/Controllers/api/MainController.php
+++ app/Http/Controllers/api/MainController.php
@@ -9,28 +9,49 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function promptQuery(Request $request){
+    public function promptQuery(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'pregunta'=> 'required',
+            'pregunta' => 'required',
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json(['message' => 'No se puede procesar la solicitud. Faltan campos'], 422);
         }
 
-try {
-    $respuesta = Http::timeout(495)->post(config("ai.model_url").'/api/generate', [
-        'model' => config("ai.app_model_name"),
-        'prompt' => $request->get('pregunta'),
-        'stream' => false
-    ]);
+        try {
+            $respuesta = Http::timeout(495)->post(config("ai.model_url") . '/api/generate', [
+                'model' => config("ai.app_model_name"),
+                'prompt' => $request->get('pregunta'),
+                'stream' => false
+            ]);
 
-    return response()->json(["respuesta" => $respuesta["response"]], 200);
+            return response()->json(["respuesta" => $respuesta["response"]], 200);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
 
-} catch (\Throwable $th) {
-    throw $th;
-}
-       
-        
+    public function gemmaLargeModel(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'pregunta' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'No se puede procesar la solicitud. Faltan campos'], 422);
+        }
+
+        try {
+            $respuesta = Http::timeout(495)->post(config("ai.model_url") . '/api/generate', [
+                'model' => config("ai.app_model_name"),
+                'prompt' => $request->get('pregunta'),
+                'stream' => false
+            ]);
+
+            return response()->json(["respuesta" => $respuesta["response"]], 200);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
