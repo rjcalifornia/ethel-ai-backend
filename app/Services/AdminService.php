6c115ae6920db73a236309bc00916199cf27d207
rjcalifornia <rodolfohernan20@gmail.com>
2024-09-11 08:21:10 -0600

    Changes to be committed: 	modified:   app/Services/AdminService.php

--- app/Services/AdminService.php
+++ app/Services/AdminService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+use App\Models\AppTokens;
 use Carbon\Carbon;
 
 use Illuminate\Support\Facades\DB;
@@ -14,15 +15,21 @@ class AdminService
     public function create($request){
         try {
             $app = new AuthorizedApps();
+            $appTokens = new AppTokens();
             $clientId = Str::uuid()->toString();
             $apiKey = Str::uuid()->toString();
-            DB::transaction(function () use ($request, $app, $clientId, $apiKey) {
+            DB::transaction(function () use ($request, $app, $clientId, $apiKey, $appTokens) {
                 $app->name = $request->get('name');
                 $app->client_id = $clientId;
                 $app->api_key = bcrypt($apiKey);
                 $app->active = true;
                 $app->role_id = 1;
                 $app->save();
+
+                $appTokens->app_id = $app->id;
+                $appTokens->tokens_allocated = 100000;
+                $appTokens->active = true;
+                $appTokens->save();
               
             });
 
